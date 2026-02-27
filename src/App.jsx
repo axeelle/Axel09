cb6921842ebbde1edad9c2463208b86d16574112
axeelle <axeldelasalas09@gmail.com>
2026-02-27 15:20:34 +0800

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import backgrounds from '../src/assets/blackbg.jpg';
 function App() {
 
   return (
-      <main className="flex flex-col h-screen bg-black bg-cover w-full">
+      <main className="flex flex-col h-screen bg-cover w-full" style={{backgroundImage: `url(${backgrounds})`}}>
         <div className="flex flex-col justify-between h-full"> 
           <nav className="flex flex-row justify-around pt-5">
             <div>
-              <h1 className=" text-white">logo</h1>
+              <h1 className=" text-orange-600 font-extrabold text-3xl ">logo</h1>
             </div>
             <div className="flex flex-row justify-center">
               <ul className="flex text-white cursor-pointer gap-5">
@@ -17,10 +18,10 @@ function App() {
               </ul>
             </div>
             <div className="">  
-              <button className=" text-white bg-orange-600py-4">hire me</button>
+              <button className=" text-white bg-orange-800 py-3 px-10 rounded-2xl">hire me</button>
             </div>
           </nav>
-          <content className="flex flex-col grow justify-center gap-5">
+          <content className="flex flex-col grow justify-center gap-5 pl-7">
             <h1 className="text-2xl text-white">hi! I am</h1>
             <h1 className="text-2xl text-white">Axel De las ALas</h1>
             <h1 className=" text-7xl text-amber-800">MY PORTFOLIO</h1>
@@ -34,7 +35,7 @@ function App() {
             </icon>
             <buttonn className="flex flex-row gap-5">
               <button className=" bg-orange-800 rounded-2xl px-6 py-2 text-white">Hire Me</button>
-              <button className="bg-orange-800 rounded-2xl px-6 py-2 text-white">Download CV</button>
+              <button className=" bg-black border rounded-2xl px-6 py-2 text-white">Download CV</button>
             </buttonn>
             <others>
                 <ul className="inline-grid grid-cols-3 border-2 border-white text-white bg-gray-900 rounded-2xl py-5 divide-x divide-white">
